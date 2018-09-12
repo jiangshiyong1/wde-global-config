37a43a8ea05341d369a09af36c487a04009df03a
jiangshiyong1 <1065089472@qq.com>
2018-09-12 16:34:23 +0800

    m

--- MonitorPlatform/Frontend/ipConfig.js
+++ MonitorPlatform/Frontend/ipConfig.js
@@ -1,11 +1,11 @@
 exports.install = function (Vue, options) {
   Vue.prototype.getServerIp = function () {
     // var serverIp = 'http://10.60.1.146:9999'
-    var serverIp = 'http://10.61.1.27:9000'
+    var serverIp = 'http://172.17.0.63:9000'
     return serverIp
   }
   Vue.prototype.getGenerateIp = function () {
-    var generateIp = 'http://10.61.1.24:8080'
+    var generateIp = 'http://172.17.0.60:8080'
     // var generateIp = 'http://10.60.1.146:8090'
     return generateIp
   }
